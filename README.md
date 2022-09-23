0301b9be8841cd64030c987df48db8b5b40e75c7
Qbandev <Qbandev@users.noreply.github.com>
2022-09-23 13:27:35 +0200

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # awssg
-Python script to check aws security group use by services in one region 
+
+Python script to check aws security group use by services in one region
+
+## Motivation
+
+There are many tools to check security group use, but I wanted to create a tool that would allow me to check the use of one specific security groups in a region, and all the services using it as possible.
+
+## About
+
+The script will check the specified security group id in one region and will return the services that use it.
+
+The script will check the following aws services:
+
+- "ec2"
+- "eni"
+- "elb"
+- "alb"
+- "rds"
+- "redshift"
+- "elasticache"
+- "eks"
+- "ecs"
+- "efs"
+- "emr"
+
+It will check all of them or the one specified in the command line.
+
+The script will return the information in the console and also in a json file in the current directory.
+
+### Prerequisites
+
+- [python3](https://www.python.org/downloads/) or higher and [pip3](https://docs.python.org/3/installing/index.html)(already installed with python3)
+
+### Installing
+
+Clone the repository to your local machine with the command:
+
+```bash
+git clone https://github.com/Qbandev/awssg.git
+```
+
+Access the directory with the command:
+
+```bash
+cd awssg
+```
+
+Install boto3 dependency in `requirements.txt` file. Install with the command:
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+Check if the script is working with the command:
+
+```bash
+python3 awssg.py --help
+```
+
+Here is the help output:
+
+```bash
+python3 sgcheck.py -h
+usage: sgcheck.py [-h] [-r REGION] [-p PROFILE] -sgid SECURITYGROUPID [-svc [{all,ec2,eni,elb,alb,rds,redshift,elasticache,eks,ecs,efs,emr}]] [-o OUTPUT]
+
+Find Security Group in AWS region
+
+options:
+  -h, --help            show this help message and exit
+  -r REGION, --region REGION
+                        The AWS region to check (default 'us-east-1')
+  -p PROFILE, --profile PROFILE
+                        The profile to use for the connection to AWS (default 'default')
+  -sgid SECURITYGROUPID, --securitygroupid SECURITYGROUPID
+                        The Security Group ID to check (required)
+  -svc [{all,ec2,eni,elb,alb,rds,redshift,elasticache,eks,ecs,efs,emr}], --service [{all,ec2,eni,elb,alb,rds,redshift,elasticache,eks,ecs,efs,emr}]
+                        AWS Service to check security group (default 'all')
+  -o OUTPUT, --output OUTPUT
+                        Set output file name (default 'sgid.json').
+```
+
+## Usage
+
+You must specify the security group id to check with the `-sgid` other options are optional and have default values.
+
+You can check all the services in one region or just one, here is a simple example of the use:
+
+```bash
+python3 sgcheck.py -p prod -sgid sg-567892f85d -svc alb -r us-east-2
+
+[
+  "albLoadBalancerName: prod-lb",
+  "albLoadBalancerName: system-staging-lb"
+]
+```
+
+The command return the alb load balancers that use the security group `sg-567892f85d` in the region `us-east-2` and the profile `prod`.
+
+The command output is also saved in a json file in the current directory with the default name `sg-567892f85d.json`.
---
+++ awssg.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+import os
+import boto3
+import argparse
+import json
+import sys
+
+try:
+    parser = argparse.ArgumentParser(
+        description="Find Security Group in AWS region")
+    parser.add_argument(
+        "-r",
+        "--region",
+        type=str,
+        default="us-east-1",
+        help="The AWS region to check (default used us-east-1)"
+    )
+    parser.add_argument(
+        "-p",
+        "--profile",
+        type=str,
+        default="default",
+        help="The profile to use for the connection to AWS (default used 'default')",
+    )
+    parser.add_argument(
+        "-sgid",
+        "--securitygroupid",
+        type=str,
+        default=None,
+        help="The Security Group ID to check (required)",
+        required=True,
+    )
+    parser.add_argument(
+        "-svc",
+        "--service",
+        action="store",
+        type=str,
+        default="all",
+        nargs="?",
+        choices=(
+            "all", "ec2", "eni", "elb", "alb", "rds", "redshift", "elasticache", "eks", "ecs", "efs", "emr"),
+        help="AWS Service to check security group (default used 'all')",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        type=str,
+        help="Set output file name (default is sg-id.json).",
+    )
+    args = parser.parse_args()
+
+    # Set boto3 environment variable to use new endpoint format.
+    # See FutureWarning https://github.com/boto/botocore/issues/2705 for more details
+    os.environ['BOTO_DISABLE_COMMONNAME'] = 'True'
+
+    # Open Boto3 session
+    session = boto3.session.Session(profile_name=args.profile)
+
+    # Create the SG dictionary to store the data
+    sg_associations = []
+
+    try:
+        # Get security group by instances
+        if args.service == "all" or args.service == "ec2":
+            ec2_client = session.client("ec2", region_name=args.region)
+            instances_dict = ec2_client.describe_instances()
+            reservations = instances_dict["Reservations"]
+            for i in reservations:
+                for j in i["Instances"]:
+                    for k in j["SecurityGroups"]:
+                        if k["GroupId"] == args.securitygroupid:
+                            sg_associations.append(
+                                "ec2InstanceId: " + i["Instances"][0]["InstanceId"])
+
+        # Security Groups in use by Network Interfaces
+        if args.service == "all" or args.service == "eni":
+            eni_dict = ec2_client.describe_network_interfaces()
+            for i in eni_dict["NetworkInterfaces"]:
+                for j in i["Groups"]:
+                    if j["GroupId"] == args.securitygroupid:
+                        sg_associations.append(
+                            "eniNetworkInterfaceId: " + i["NetworkInterfaceId"])
+
+        # Security groups used by classic ELBs
+        if args.service == "all" or args.service == "elb":
+            elb_client = session.client("elb", region_name=args.region)
+            elb_dict = elb_client.describe_load_balancers()
+            for i in elb_dict["LoadBalancerDescriptions"]:
+                for j in i["SecurityGroups"]:
+                    if j == args.securitygroupid:
+                        sg_associations.append(
+                            "elbLoadBalancerName: " + i["LoadBalancerName"])
+
+        # Security groups used by ALBs
+        if args.service == "all" or args.service == "alb":
+            elb2_client = session.client("elbv2", region_name=args.region)
+            elb2_dict = elb2_client.describe_load_balancers()
+            for i in elb2_dict["LoadBalancers"]:
+                if "SecurityGroups" in i.keys():
+                    for j in i["SecurityGroups"]:
+                        if j == args.securitygroupid:
+                            sg_associations.append(
+                                "albLoadBalancerName: " + i["LoadBalancerName"])
+
+        # Security groups used by RDS
+        if args.service == "all" or args.service == "rds":
+            rds_client = session.client("rds", region_name=args.region)
+            rds_dict = rds_client.describe_db_instances()
+            for i in rds_dict["DBInstances"]:
+                for j in i["VpcSecurityGroups"]:
+                    if j["VpcSecurityGroupId"] == args.securitygroupid:
+                        sg_associations.append(
+                            "rdsDBInstanceIdentifier: " + i["DBInstanceIdentifier"])
+
+        # Security groups used by Redshift
+        if args.service == "all" or args.service == "redshift":
+            redshift_client = session.client(
+                "redshift", region_name=args.region)
+            redshift_dict = redshift_client.describe_clusters()
+            for i in redshift_dict["Clusters"]:
+                for j in i["VpcSecurityGroups"]:
+                    if j["VpcSecurityGroupId"] == args.securitygroupid:
+                        sg_associations.append(
+                            "redshiftClusterIdentifier: " + i["ClusterIdentifier"])
+
+        # Security groups used by Elasticache
+        if args.service == "all" or args.service == "elasticache":
+            elasticache_client = session.client(
+                "elasticache", region_name=args.region)
+            elasticache_dict = elasticache_client.describe_cache_clusters()
+            for i in elasticache_dict["CacheClusters"]:
+                for j in i["SecurityGroups"]:
+                    if j["SecurityGroupId"] == args.securitygroupid:
+                        sg_associations.append(
+                            "elasticacheCacheClusterId: " + i["CacheClusterId"])
+
+        # Security groups used by EKS
+        if args.service == "all" or args.service == "eks":
+            eks_client = session.client("eks", region_name=args.region)
+            eks_dict = eks_client.list_clusters()
+            for i in eks_dict["clusters"]:
+                eks_sg_dict = eks_client.describe_cluster(name=i)
+                for j in eks_sg_dict["cluster"]["resourcesVpcConfig"]["securityGroupIds"]:
+                    if j == args.securitygroupid:
+                        sg_associations.append("eksClusterId: " + i)
+
+        # Security groups used by ECS
+        if args.service == "all" or args.service == "ecs":
+            ecs_client = session.client("ecs", region_name=args.region)
+            ecs_dict = ecs_client.list_clusters()
+            for i in ecs_dict["clusterArns"]:
+                ecs_svc_dict = ecs_client.list_services(cluster=i)
+                for k in ecs_svc_dict["serviceArns"]:
+                    ecs_sg_dict = ecs_client.describe_services(
+                        cluster=i, services=[k])
+                    for j in ecs_sg_dict["services"][0]["networkConfiguration"]["awsvpcConfiguration"]["securityGroups"]:
+                        if j == args.securitygroupid:
+                            sg_associations.append(
+                                "ecsClusterId: " + i.split(":cluster/", 1)[1] + " ecsServiceName: " + ecs_sg_dict["services"][0]["serviceArn"].split(":service/")[1])
+
+        # Security groups used by EFS
+        if args.service == "all" or args.service == "efs":
+            efs_client = session.client("efs", region_name=args.region)
+            efs_dict = efs_client.describe_file_systems()
+            for i in efs_dict["FileSystems"]:
+                efs_mount_dict = efs_client.describe_mount_targets(
+                    FileSystemId=i["FileSystemId"])
+                for j in efs_mount_dict["MountTargets"]:
+                    efs_dict_sg = efs_client.describe_mount_target_security_groups(
+                        MountTargetId=j["MountTargetId"])
+                    for k in efs_dict_sg["SecurityGroups"]:
+                        if k == args.securitygroupid:
+                            sg_associations.append(
+                                "efsFileSystemId: " + j["FileSystemId"] + " efsMountTargetId: " + j["MountTargetId"])
+
+        # Security groups used by EMR
+        if args.service == "all" or args.service == "emr":
+            emr_client = session.client("emr", region_name=args.region)
+            emr_dict = emr_client.list_clusters()
+            for i in emr_dict["Clusters"]:
+                if i["Status"]["State"] == "WAITING":
+                    emr_sg_dict = emr_client.describe_cluster(
+                        ClusterId=i["Id"])
+                    # Get the security group ID from the EMR cluster
+                    if emr_sg_dict["Cluster"]["Ec2InstanceAttributes"]["EmrManagedMasterSecurityGroup"] == args.securitygroupid:
+                        sg_associations.append(
+                            "emrClusterName: " + i["Name"])
+                    if emr_sg_dict["Cluster"]["Ec2InstanceAttributes"]["EmrManagedSlaveSecurityGroup"] == args.securitygroupid:
+                        sg_associations.append(
+                            "emrClusterName: " + i["Name"])
+                    if emr_sg_dict["Cluster"]["Ec2InstanceAttributes"]["AdditionalMasterSecurityGroups"] == args.securitygroupid:
+                        sg_associations.append(
+                            "emrClusterName: " + i["Name"])
+                    if emr_sg_dict["Cluster"]["Ec2InstanceAttributes"]["AdditionalSlaveSecurityGroups"] == args.securitygroupid:
+                        sg_associations.append(
+                            "emrClusterName: " + i["Name"])
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    # Save the results to a file
+    if args.output == None:
+        file = open(args.securitygroupid+".json", "w")
+    else:
+        file = open(args.output, "w")
+
+    if sg_associations == []:
+        print("No associations found for:" +
+              args.securitygroupid + " in region " + args.region)
+        file.write("No associations found for:" +
+                   args.securitygroupid + " in region " + args.region)
+        file.close()
+    else:
+        print(json.dumps(sg_associations, indent=2, sort_keys=True))
+        file.write(json.dumps(sg_associations, indent=2, sort_keys=True))
+        file.close()
+
+except KeyboardInterrupt:
+    print("\nCtrl+C Caught, Terminating")
